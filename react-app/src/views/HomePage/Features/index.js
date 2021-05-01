980940f110848ca9d46098c436266dff17ba63d6
sudo-vaibhav <mailvaibhavchopra@gmail.com>
2021-05-01 21:55:23 +0530

    minor changes

--- react-app/src/views/HomePage/Features/index.js
+++ react-app/src/views/HomePage/Features/index.js
@@ -1,3 +1,27 @@
+import bell from '../../../trinity/bell.svg'
+import diamond from '../../../trinity/diamond.svg'
+import house from '../../../trinity/house.svg'
+
+const featuresData = [
+  {
+    asset: bell,
+    name: '80G tax exemption',
+    description: 'Donations are tax deductible (80G)',
+  },
+  {
+    asset: diamond,
+    name: 'NFTs',
+    description:
+      'Top donors will be provided with a Unique NFT which can be traded further or used to avail attractive items in real world ',
+  },
+  {
+    asset: house,
+    name: 'Minimial platform fees',
+    description:
+      'With just less than 2% platform fees, you can continue doing good without having to worry about burning a hole through your pocket',
+  },
+]
+
 const Features = () => {
   return (
     <section className="container mx-auto text-center my-10">
@@ -10,26 +34,22 @@ const Features = () => {
         integer elementum eu iaculis fringilla elementum augue.
       </p>
       <div className="grid grid-rows-3 gap-3 grid-cols-1 lg:grid-rows-1 lg:grid-cols-3  lg:w-3/4 mx-auto my-10">
-        {[
-          {
-            name: 'Feature 1',
-            description:
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Leo massacurabitur aliquet mattis quis tellus ullamcorper arcu est. Vulputatevel integer elementum eu iaculis fringilla elementum augue.',
-          },
-          {
-            name: 'Feature 2',
-            description:
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Leo massacurabitur aliquet mattis quis tellus ullamcorper arcu est. Vulputatevel integer elementum eu iaculis fringilla elementum augue.',
-          },
-          {
-            name: 'Feature 3',
-            description:
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Leo massacurabitur aliquet mattis quis tellus ullamcorper arcu est. Vulputatevel integer elementum eu iaculis fringilla elementum augue.',
-          },
-        ].map((e) => {
+        {featuresData.map((e) => {
           return (
-            <div className="rounded-md bg-light-100 p-4 text-left" key={e.name}>
-              <h4 className="text-light-900 my-3">{e.name}</h4>
+            <div
+              className="rounded-md text-center bg-light-100 p-4 text-left"
+              key={e.name}
+            >
+              <img
+                src={e.asset}
+                className="mx-auto"
+                style={{
+                  maxHeight: 80,
+                  maxWidth: 80,
+                }}
+              />
+
+              <h4 className="text-lg font-medium py-4">{e.name}</h4>
               <p>{e.description}</p>
             </div>
           )
